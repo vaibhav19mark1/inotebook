c1015b96b491443f1f8927a9ace82310c36581e0
vaibhav19mark1 <54894553+vaibhav19mark1@users.noreply.github.com>
2023-01-11 23:48:59 +0530

    password encryption and JWT

--- inotebook-backend/routes/auth.js
+++ inotebook-backend/routes/auth.js
@@ -1,7 +1,11 @@
 const express = require("express");
 const User = require("../models/Users");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
+const jwt = require('jsonwebtoken');
 const { body, validationResult } = require("express-validator");
+
+const JWT_SECRET="iamgoingtobuyalamborghini";
 
 // create user using: POST "/api/auth/createuser". No login required
 router.post(
@@ -17,19 +21,29 @@ router.post(
     if (!errors.isEmpty()) {
       return res.status(400).json({ errors: errors.array() });
     }
-    // Check if the user with this email already exists
     try {
+      // Check if the user with this email already exists
       let user = await User.findOne({ email: req.body.email });
       if (user) {
         return res.status(400).json({ error: "User with this email already exists" });
       }
       // Create a new user
+      const salt = await bcrypt.genSalt(10);
+      const secPass = await bcrypt.hash(req.body.password, salt);
       user = await User.create({
         name: req.body.name,
         email: req.body.email,
-        password: req.body.password,
+        password: secPass,
       });
-      res.json(user);
+      const data={
+        user:{
+          id: user.id
+        }
+      }
+      const authtoken=jwt.sign(data,JWT_SECRET);
+
+      // res.json(user);
+      res.json({authtoken});
     } catch (error) {
       console.log(error.message);
       res.status(500).send("Some error occured!");
