b82c2c60e0bba62dc233d83026dead98b6fcfb83
vaibhav19mark1 <54894553+vaibhav19mark1@users.noreply.github.com>
2023-01-12 20:56:19 +0530

    update note api endpoint added

--- inotebook-backend/models/Note.js
+++ inotebook-backend/models/Note.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
 const NotesSchema = new Schema({
-    title:{
+    user:{
         type: mongoose.Schema.Types.ObjectId,
         ref: 'user'
     },
@@ -24,5 +24,5 @@ const NotesSchema = new Schema({
     }
 });
 
-const Note =mongoose.model('notes',NotesSchema);
+const Note = mongoose.model('notes',NotesSchema);
 module.exports = Note;
--- inotebook-backend/routes/notes.js
+++ inotebook-backend/routes/notes.js
@@ -15,7 +15,7 @@ router.get("/fetchallnotes", fetchUser, async (req, res) => {
   }
 });
 
-// ROUTE 2: Adda new note: POST '/api/notes/addnote'. Login Required
+// ROUTE 2: Add a new note: POST '/api/notes/addnote'. Login Required
 router.post(
   "/addnote",
   fetchUser,
@@ -45,5 +45,34 @@ router.post(
     }
   }
 );
+
+// ROUTE 3: Update an exisiting note: PUT '/api/notes/updatenote'. Login Required
+router.put("/updatenote/:id", fetchUser, async (req, res) => {
+  const { title, description, tag } = req.body;
+
+  // Create a new note
+  const newNote = {};
+  if (title) {
+    newNote.title = title;
+  }
+  if (description) {
+    newNote.description = description;
+  }
+  if (tag) {
+    newNote.tag = tag;
+  }
+
+  // Find the note to be updated and update it
+  let note = await Note.findById(req.params.id);
+  if (!note) {
+    return res.status(404).send("Not Found");
+  }
+  if (note.user.toString() !== req.user.id) {
+    return res.status(401).send("Not Authorized to update");
+  }
+
+  note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+  res.json({ note });
+});
 
 module.exports = router;
