bb79404823583d343308dd9a734b9d19676a324a
vaibhav19mark1 <54894553+vaibhav19mark1@users.noreply.github.com>
2023-01-13 15:06:36 +0530

    Delete note api endpoint added

--- inotebook-backend/routes/notes.js
+++ inotebook-backend/routes/notes.js
@@ -49,30 +49,53 @@ router.post(
 // ROUTE 3: Update an exisiting note: PUT '/api/notes/updatenote'. Login Required
 router.put("/updatenote/:id", fetchUser, async (req, res) => {
   const { title, description, tag } = req.body;
+  try {
+    // Create a new note
+    const newNote = {};
+    if (title) {
+      newNote.title = title;
+    }
+    if (description) {
+      newNote.description = description;
+    }
+    if (tag) {
+      newNote.tag = tag;
+    }
+    // Find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+    // Allow the user to update the note if he owns it
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Authorized to update");
+    }
+    note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
+    res.json({ note });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal Server Error");
+  }
+});
 
-  // Create a new note
-  const newNote = {};
-  if (title) {
-    newNote.title = title;
+// ROUTE 4: Delete an exisiting note: DELETE '/api/notes/deletenote'. Login Required
+router.delete("/deletenote/:id", fetchUser, async (req, res) => {
+  try {
+    // Find the note to be updated and update it
+    let note = await Note.findById(req.params.id);
+    if (!note) {
+      return res.status(404).send("Not Found");
+    }
+    // Allow the user to update the note if he owns it
+    if (note.user.toString() !== req.user.id) {
+      return res.status(401).send("Not Authorized to update");
+    }
+    note = await Note.findByIdAndDelete(req.params.id);
+    res.json({ Success: "Note ha been deleted", note: note });
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send("Internal Server Error");
   }
-  if (description) {
-    newNote.description = description;
-  }
-  if (tag) {
-    newNote.tag = tag;
-  }
-
-  // Find the note to be updated and update it
-  let note = await Note.findById(req.params.id);
-  if (!note) {
-    return res.status(404).send("Not Found");
-  }
-  if (note.user.toString() !== req.user.id) {
-    return res.status(401).send("Not Authorized to update");
-  }
-
-  note = await Note.findByIdAndUpdate(req.params.id, { $set: newNote }, { new: true });
-  res.json({ note });
 });
 
 module.exports = router;
